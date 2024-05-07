d210508bdf74ca23ddb97995e6ff47490ea16b02
Facu <facundojsole@gmail.com>
2024-05-07 16:54:01 -0300

    quick style fixes and new animation

--- src/app/components/Paragraph.tsx
+++ src/app/components/Paragraph.tsx
@@ -1,50 +1,51 @@
-"use client"
+"use client";
 
-import { ReactNode, useRef } from "react"
-import { useScroll, motion, useTransform } from "framer-motion"
+import { ReactNode, useRef } from "react";
+import { useScroll, motion, useTransform } from "framer-motion";
 
 type ParagraphProps = {
-    value: string
-}
+  value: string;
+};
 
-export default function Paragraph( { value }: ParagraphProps  ) {
+export default function Paragraph({ value }: ParagraphProps) {
+  const element = useRef(null);
 
-    const element = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: element,
+    offset: ["start 0.8", "start 0.1"],
+  });
 
-    const { scrollYProgress } = useScroll({
-        target: element,
-        offset: ['start 0.8', 'start 0.1']
-    })
+  const words = value.split(" ");
 
-    const words = value.split(" ")
+  return (
+    <p
+      className="text-white text-[4dvh] lg:text-[5dvh] leading-tight flex flex-wrap relative lg:max-w-[25ch]"
+      ref={element}
+    >
+      {words.map((word, i) => {
+        const start = i / words.length;
+        const end = start + 1 / words.length;
 
-    return (
-        <p className="text-white text-[5dvh] leading-tight flex flex-wrap relative" ref={element}>
-            {
-                words.map( (word, i) => {
-                    const start = i / words.length;
-                    const end = start + ( 1 / words.length )
-
-                    return <span key={i} className="relative mr-4">
-                        <span className="absolute opacity-20">{word}</span>
-                        <Word range={[start,end]} progress={scrollYProgress}>{word}</Word>
-                    </span>
-                })
-            }
-        </p>
-    )
-
+        return (
+          <span key={i} className="relative mr-4">
+            <span className="absolute opacity-20">{word}</span>
+            <Word range={[start, end]} progress={scrollYProgress}>
+              {word}
+            </Word>
+          </span>
+        );
+      })}
+    </p>
+  );
 }
 
 type WordProps = {
-    children: ReactNode
-    range: Array<number>
-    progress: any
-}
+  children: ReactNode;
+  range: Array<number>;
+  progress: any;
+};
 
 const Word = ({ children, range, progress }: WordProps) => {
-    const opacity = useTransform(progress, range, [0,1])
-    return (
-        <motion.span style={{opacity: opacity}}>{children}</motion.span>
-    )
-}
+  const opacity = useTransform(progress, range, [0, 1]);
+  return <motion.span style={{ opacity: opacity }}>{children}</motion.span>;
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,108 @@
-"use client"
+"use client";
 import Lenis from "lenis";
-import { motion } from "framer-motion"
+import { easeIn, motion } from "framer-motion";
 import Paragraph from "./components/Paragraph";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 import { useEffect } from "react";
 
-
 export default function Home() {
-
   useEffect(() => {
-    const lenis = new Lenis()
+    const lenis = new Lenis();
 
     function raf(time: any) {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
+      lenis.raf(time);
+      requestAnimationFrame(raf);
     }
-    requestAnimationFrame(raf)  
-  }, [])
+    requestAnimationFrame(raf);
+  }, []);
 
-  
+  const creativityArr = "creativity".split("");
+
   return (
-    <main className="[&>section]:p-24 [&>section]:grid [&>section]:place-items-center [&>*:nth-child(even)]:bg-[#191919] relative">
+    <main className="[&>section]:p-12 md:[&>section]:p-24 [&>section]:grid [&>section]:place-items-center [&>*:nth-child(even)]:bg-[#191919] relative">
       <section className="h-screen w-screen relative overflow-hidden">
         <motion.h1
-          className="text-5xl md:text-7xl"
-          initial={{opacity: 0}}
-          animate={{opacity: 1}}
-          transition={{duration: 2}}
+          className="text-5xl md:text-7xl text-center"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
         >
-          Embrace <span className="text-[#5238f4]">creativity</span>
+          Embrace <br className="lg:hidden" />
+          <span className="text-[#5238f4]">
+            {creativityArr.map((letter, i) => {
+              return (
+                <motion.span
+                  initial={{
+                    y: 0,
+                  }}
+                  animate={{
+                    y: [0, -5, 0],
+                    transition: {
+                      duration: 0.3,
+                      ease: [0.61, 1, 0.88, 1],
+                      delay: i * 0.2 + 4.4,
+                      repeat: Infinity,
+                    },
+                  }}
+                  key={i}
+                  style={{ display: "inline-block" }}
+                >
+                  {letter}
+                </motion.span>
+              );
+            })}
+          </span>
         </motion.h1>
-        <svg className="absolute w-full md:w-auto top-10 lg:top-0 lg:right-16 -rotate-12" width="1550" height="128" viewBox="0 0 1510 128" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <motion.path d="M3 124.16C96.984 124.16 96.984 3.84009 190.968 3.84009C284.952 3.84009 284.952 124.16 378.936 124.16C472.92 124.16 472.904 3.84009 566.856 3.84009C660.808 3.84009 660.84 124.16 754.808 124.16C848.776 124.16 848.824 3.84009 942.84 3.84009C1036.86 3.84009 1036.82 124.16 1130.81 124.16C1224.79 124.16 1224.86 3.84009 1318.9 3.84009C1412.95 3.84009 1412.95 124.16 1507 124.16" stroke="#5238F4" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" initial={{pathLength: 0, rotate: 180}} animate={{pathLength: 1, transition: {
-            duration: 2,
-            delay: 1,
-            ease: "easeInOut"
-          }}}/>
+        <svg
+          className="absolute w-full md:w-auto -top-10 -left-12 -rotate-45 lg:top-0 lg:-left-10 lg:-rotate-12"
+          width="1550"
+          height="128"
+          viewBox="0 0 1550 128"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.path
+            d="M3 124.16C96.984 124.16 96.984 3.84009 190.968 3.84009C284.952 3.84009 284.952 124.16 378.936 124.16C472.92 124.16 472.904 3.84009 566.856 3.84009C660.808 3.84009 660.84 124.16 754.808 124.16C848.776 124.16 848.824 3.84009 942.84 3.84009C1036.86 3.84009 1036.82 124.16 1130.81 124.16C1224.79 124.16 1224.86 3.84009 1318.9 3.84009C1412.95 3.84009 1412.95 124.16 1507 124.16"
+            stroke="#5238F4"
+            strokeWidth="6"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ pathLength: 0, rotate: 180 }}
+            animate={{
+              pathLength: 1,
+              transition: {
+                duration: 2,
+                delay: 1,
+                ease: "easeInOut",
+              },
+            }}
+          />
         </svg>
-        <svg className="absolute bottom-5 w-full md:w-auto lg:left-10 lg:bottom-12 -rotate-12" width="1550" height="128" viewBox="0 0 1510 128" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <motion.path d="M3 124.16C96.984 124.16 96.984 3.84009 190.968 3.84009C284.952 3.84009 284.952 124.16 378.936 124.16C472.92 124.16 472.904 3.84009 566.856 3.84009C660.808 3.84009 660.84 124.16 754.808 124.16C848.776 124.16 848.824 3.84009 942.84 3.84009C1036.86 3.84009 1036.82 124.16 1130.81 124.16C1224.79 124.16 1224.86 3.84009 1318.9 3.84009C1412.95 3.84009 1412.95 124.16 1507 124.16" stroke="#5238F4" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" initial={{pathLength: 0}} animate={{pathLength: 1, transition: {
-            duration: 2,
-            delay: 2.2,
-            ease: "easeInOut"
-          }}}/>
-        </svg>     
+        <svg
+          className="absolute -bottom-6 -right-16 w-full md:w-auto lg:-right-16 lg:bottom-10 -rotate-12"
+          width="1550"
+          height="128"
+          viewBox="0 0 1550 128"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.path
+            d="M3 124.16C96.984 124.16 96.984 3.84009 190.968 3.84009C284.952 3.84009 284.952 124.16 378.936 124.16C472.92 124.16 472.904 3.84009 566.856 3.84009C660.808 3.84009 660.84 124.16 754.808 124.16C848.776 124.16 848.824 3.84009 942.84 3.84009C1036.86 3.84009 1036.82 124.16 1130.81 124.16C1224.79 124.16 1224.86 3.84009 1318.9 3.84009C1412.95 3.84009 1412.95 124.16 1507 124.16"
+            stroke="#5238F4"
+            strokeWidth="6"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ pathLength: 0 }}
+            animate={{
+              pathLength: 1,
+              transition: {
+                duration: 2,
+                delay: 2.2,
+                ease: "easeInOut",
+              },
+            }}
+          />
+        </svg>
       </section>
       <section className="h-screen w-screen bg-[#191919]">
         <Paragraph value="Creative websites that really catch users' eyes are what work best. Shine among others!" />
